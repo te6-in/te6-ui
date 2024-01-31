8790145bdb6a473753de6b8c7da8970632b656b8
Chanhwi Joo <56245920+te6-in@users.noreply.github.com>
2024-01-31 18:16:09 +0900

    fix: fix tab bar item style

--- src/layout/TabBar/TabBarItem.tsx
+++ src/layout/TabBar/TabBarItem.tsx
@@ -20,7 +20,7 @@ export function TabBarItem({ icon: Icon, text, href }: TabBarItemProps) {
   return (
     <Link
       className={j(
-        "flex flex-col items-center justify-center gap-1 rounded-xl ring-inset transition-all focus:outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-accent-500 focus-visible:ring-offset-2 focus-visible:ring-offset-body active:scale-95 dark:focus-visible:ring-offset-body-dark sm:flex-row sm:justify-start sm:gap-2 sm:p-4 sm:hover:bg-base-200 dark:sm:hover:bg-base-dark-900",
+        "flex h-full flex-col items-center justify-center gap-1 rounded-xl ring-inset transition-all focus:outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-accent-500 focus-visible:ring-offset-2 focus-visible:ring-offset-body active:scale-95 dark:focus-visible:ring-offset-body-dark sm:flex-row sm:justify-start sm:gap-2 sm:p-4 sm:hover:bg-base-200 dark:sm:hover:bg-base-dark-900",
         didMatch ? "text-primary-500" : "text-base-700 dark:text-base-dark-300",
       )}
       href={href}
