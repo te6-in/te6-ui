ff8e06e4400baecc0e54021d3a4334be4fbf8c1c
Chanhwi Joo <56245920+te6-in@users.noreply.github.com>
2024-02-07 20:41:54 +0900

    fix: fix `safe-area-inset-bottom` not working properly / close #4

--- src/layout/FloatingButton.tsx
+++ src/layout/FloatingButton.tsx
@@ -18,7 +18,7 @@ export function FloatingButton({
   return (
     <Link
       className={j(
-        "fixed right-4 z-10 flex items-center justify-center gap-2 rounded-2xl border-primary-600 bg-primary-600 p-4 font-semibold text-base-50 shadow-md transition-all hover:border-primary-700 hover:bg-primary-700 focus:outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-accent-500 focus-visible:ring-offset-2 focus-visible:ring-offset-body active:scale-95 dark:border-primary-400 dark:bg-primary-400 dark:text-base-dark-950 dark:hover:border-primary-300 dark:hover:bg-primary-300 dark:focus-visible:ring-offset-body-dark sm:bottom-6 sm:right-1/2 sm:translate-x-[calc(50%+6rem)]",
+        "fixed right-4 z-10 flex items-center justify-center gap-2 rounded-2xl border-primary-600 bg-primary-600 p-4 font-semibold text-base-50 shadow-md transition-all hover:border-primary-700 hover:bg-primary-700 focus:outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-accent-500 focus-visible:ring-offset-2 focus-visible:ring-offset-body active:scale-95 dark:border-primary-400 dark:bg-primary-400 dark:text-base-dark-950 dark:hover:border-primary-300 dark:hover:bg-primary-300 dark:focus-visible:ring-offset-body-dark sm:bottom-[calc(1.5rem+env(safe-area-inset-bottom))] sm:right-1/2 sm:translate-x-[calc(50%+6rem)]",
         isTabBarHiddenOnMobile
           ? "bottom-[calc(1rem+env(safe-area-inset-bottom))]"
           : "bottom-[calc(5rem+env(safe-area-inset-bottom))]",
